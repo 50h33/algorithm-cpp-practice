ebf682f8c6070c6d5dfea597718f3c5146bc91ab
sohee52 <wneh2023@naver.com>
2026-03-10 03:26:33 +0900

    refactor: '나머지' 다른 풀이 방법 추가

--- class1/3052.cpp
+++ class1/3052.cpp
@@ -4,14 +4,28 @@ int a[14], cnt;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	for (int i = 0; i < 10; i++) {
-        cin >> a[i];
-        a[i] %= 42;
-        bool flag = 1;
-        for (int j = 0; j < i; j++) {
-            if (a[i] == a[j]) flag = 0;
-        }
-        if (flag) cnt++;
+
+    // 방법 1 : for문 활용
+	// for (int i = 0; i < 10; i++) {
+    //     cin >> a[i];
+    //     a[i] %= 42;
+    //     bool flag = 1;
+    //     for (int j = 0; j < i; j++) {
+    //         if (a[i] == a[j]) flag = 0;
+    //     }
+    //     if (flag) cnt++;
+    // }
+    // cout << cnt << "\n";
+
+    // 방법 2 : set 활용
+    set<int> s;
+    
+    for (int i = 0; i < 10; i++) {
+        int x;
+        cin >> x;
+        s.insert(x % 42);
     }
-    cout << cnt << "\n";
+
+    cout << s.size() << '\n';
+    return 0;
 }
